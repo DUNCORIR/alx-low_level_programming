1be613ba8f54dfe78312fedc7ce5f4c23aade8aa
duncorir <duncorir@mail.com>
2024-06-20 23:08:17 +0300

    program that prints exactly Programming is like building a multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
-/* program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line
-.*/
 #include <stdio.h>
+/**
+ * main - Entry point of the program
+ * Description: This function prints a specific string using puts
+ * Return: Always returns 0 (Success)
+ */
 int main(void)
 {
 puts("Programming is like building a multilingual puzzle");
 return (0);
+
 }

92192d42b328a3a9011c8ce8cf6e3ae21384d298
duncorir <duncorir@mail.com>
2024-06-20 21:39:33 +0300

    C program that prints the phrase “with proper grammar, but the outcome is a piece of art,

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-/*prints with proper grammar, but the outcome is a piece of art,, followed by a new line*/
+/*prints with proper grammar, but the outcome is a piece of arti,*/
 /* Return: Always 0 (Success)*/
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
-return 0;
-}	
+return (0);
+
+}

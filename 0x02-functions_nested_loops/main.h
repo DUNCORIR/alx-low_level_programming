e725d369bb28815eb581e7f0655e7c1ce3f20997
duncorir <duncorir@mail.com>
2024-06-25 22:54:02 +0300

    function that checks for lowercase character.

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,5 +4,8 @@
 int _putchar(char c);
 /* Declares :print_alphabet function*/
 void print_alphabet(void);
+/* print_alphabet_x10 function */
 void print_alphabet_x10(void);
+/* _islower function */
+int _islower(int c);
 #endif 

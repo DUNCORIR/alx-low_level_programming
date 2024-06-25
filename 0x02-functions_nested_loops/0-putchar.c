023aafda118fd305084d08ea57f66bfff65a3cf2
duncorir <duncorir@mail.com>
2024-06-25 13:03:59 +0300

    program that prints _putchar, followed by a new line.

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,8 @@
 #include "main.h"
-/*main.h -custom header file has the prototype for the _putchar function*/
+ /**
+ *main.h -custom header file has the prototype for the _putchar function
+ *Return: Always 0 (Success)
+ */
 
 int main(void)
 /*main - entry point of program */

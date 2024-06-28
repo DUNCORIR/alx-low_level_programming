9d8321d6ff5d85c8198ddfc2a94de64dd2519c13
duncorir <duncorir@mail.com>
2024-06-28 14:45:40 +0300

    function that prints an integer.

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,32 +1,28 @@
 #include "main.h"
 /**
- * print_number - Prints an integer using _putchar function only.
- * main - Entry point 
+ * print_number - Prints an integer using _putchar function only. 
  * @n: The integer to be printed.
  */
 
 void print_number(int n)
-
 {
-/* Handle negative numbers */
-if (n < 0) 
-{
-_putchar('-');
-n = -n;
-
-}
-if (n == 0)
-{
-_putchar('0');
-return;
-}
-/*Recursive call to handle digits from left to right*/
-if (n / 10 != 0)
-{
-print_number(n / 10); 
-
-}
-_putchar(n % 10 + '0');
-/* Print each digit by converting it to character.*/
-
+  /* Handle negative numbers */
+  if (n < 0) 
+  { 
+   _putchar('-');
+   n = -n;
+  }
+  /* Handle zero */
+  if (n == 0)
+  {
+   _putchar('0');
+   return;
+  }
+  /*Recursive call to handle digits from left to right*/
+  if (n / 10 != 0)
+  {
+   print_number(n / 10); 
+  }
+  /* Print each digit by converting it to character.*/
+   _putchar(n % 10 + '0');
 }

fcac77fc2f196b05366dd3f793c854e16eddae44
duncorir <duncorir@mail.com>
2024-07-08 21:58:10 +0300

    function that locates a character in a string.

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,23 +11,23 @@
  */
 char *_strchr(char *s, char c)
 {
-	/* iterates thro' string for not equals to NULL */
-	while (*s != '\0')
-	{
-		/* Checks if current characteristic matches c */
-		if (*s == c)
-		{
-		/* Returns pointer to thr first occurence of c */
-			return (s);
-		}
-		s++;
-	}
-	/* checks the null terminator char */
+    /* iterates thro' string for not equals to NULL */
+    while (*s != '\0')
+    {
+        /* Checks if current characteristic matches c */
 	if (*s == c)
 	{
-	/* Returns terminator to null terminator of c if c is '\0' */
-	return (s);
+	/* Returns pointer to thr first occurence of c */
+	    return (s);
 	}
-	/* Returns NULL if c is not found in the string */
-	return (NULL);
+	s++;
+    }
+    /* checks the null terminator char */
+    if (*s == c)
+    {
+    /* Returns terminator to null terminator of c if c is '\0' */
+        return (s);
+    }
+    /* Returns NULL if c is not found in the string */
+    return (NULL);
 }

08e45a38df63820acb5146cea29096a0456e1171
duncorir <duncorir@mail.com>
2024-07-09 11:53:38 +0300

    function that prints a chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_chessboard - function to print chessboard
  * @a: represents a 2D array of a chessboard with 8 rows 8 coumns.
@@ -11,8 +10,8 @@ void print_chessboard(char (*a)[8])
 	int i , j;
 	/* iterates through row i */
 	for (i = 0; i < 8; i++)
-	{    
-	        /* iterates through j columns */
+	{
+		/* iterates through j columns */
 		for (j = 0; j < 8; j++)
 		{
 			_putchar(a[i][j]);
@@ -20,5 +19,4 @@ void print_chessboard(char (*a)[8])
 		/* prints next line */
 		_putchar('\n');
 	}
-
 }

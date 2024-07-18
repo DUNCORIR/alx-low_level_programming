07114a1620ac0a1e77d59ed2568692a03f45e1b1
duncorir <duncorir@mail.com>
2024-07-18 20:38:35 +0300

    function that allocates memory using malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -6,19 +6,19 @@
 /**
  * malloc_checked - Function that allocates memory using malloc.
  *
- * @b: Pointer to allocated memory
+ * @b: Allocated memory
  *
  * Return: A pointer to allocated memory,otherwise malloc_checked
  * function cause normal process termination with status of 98.
  */
 void *malloc_checked(unsigned int b)
 {	
-	void *malloc_checked = malloc(b);
+	void *ptr = malloc(b);
 	
-	if (malloc_checked == NULL)
+	if (ptr == NULL)
 	{
 		exit(98);
 	}
 
-	return (malloc_checked);
+	return (ptr);
 }

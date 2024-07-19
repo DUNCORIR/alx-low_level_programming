d14420fc859d9aef5ea4a5c6e5a74c9bea143260
duncorir <duncorir@mail.com>
2024-07-19 11:38:38 +0300

    function that allocates memory for an array, using malloc.

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -3,29 +3,34 @@
 #include <string.h>
 
 /**
- * calloc - Function that allocates memory for ana array using malloc.
+ * _calloc - Function that allocates memory for ana array using malloc.
  * @nmemb: Number of elements.
- * @size: size of each element.
+ * @size: Size of each element.
  *
- * Returns: Pointer to allocated memory.
+ * Return: Pointer to allocated memory, or NULL if size and nmemb is 0.
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int result_size;
 	void *allocated_memory;
+
 	if (nmemb == 0 || size == 0)
 	{
 		return (NULL);
 	}
+
 	/* Total memory allocated */
 	result_size = nmemb * size;
 	allocated_memory = malloc(result_size);
+
 	/* allocation failure checked */
 	if (allocated_memory == NULL)
 	{
 		return (NULL);
 	}
+
 	/* resets allocated memory to 0.*/
 	memset(allocated_memory, 0, result_size);
+
 	return (allocated_memory);
 }

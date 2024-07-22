f85b8d0775477c8f0838b8ee7dc316e2491fbb20
duncorir <duncorir@mail.com>
2024-07-22 22:52:55 +0300

    new type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- *  struct dog - A structure defining a dog. 
+ * struct dog - A structure defining a dog. 
  * @name: Name of the dog.
- * @owner: owner of the dog
+ * @owner: Owner of the dog
  * @age: The age of the dog
  */
 struct dog

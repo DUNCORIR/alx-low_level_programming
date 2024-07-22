fc37bbde20a2aa5c69d7fefea79ecbe0c208b4b6
duncorir <duncorir@mail.com>
2024-07-22 20:50:52 +0300

    program that prints the name of the file it was compiled from, followed by a new line.

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Program entry point
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	/*Expands to the name of the current input file as string */
+	printf ("%s\n", __FILE__);
+	return (0);
+}

3b3cc68f83cc6c37969f2ab8a393f6792f12fd48
duncorir <duncorir@mail.com>
2024-07-23 17:28:01 +0300

    new type dog_t as a new name for the type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,7 +7,8 @@
  * @age: Second member
  * @owner: Third member
  *
- * Description: Longer description
+ * Description: Structure is used to store information about a dog.
+ * it is for record purposes.
  */
 struct dog
 {
@@ -15,6 +16,13 @@ struct dog
 	float age;
 	char *owner;
 };
+
+/**
+ * dog_t - A typedef for struct dog
+ */
+
+typedef struct dog dog_t;
+
 /**
  * init_dog - Function that initializes dog structure.
  * @d: points to structure of dog
@@ -22,8 +30,31 @@ struct dog
  * @age: dog's age
  * @owner: dog's owner
  *
- * Description: long described
+ * Description: The function takes pointer to a struct dog,
+ * initializes its members with provided values then assigns
+ * name, age, and owner to members of struct dog.
  */
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
+/**
+ *  * print_dog - prints a struct dog
+ * @d: pointer to the struct dog to print
+ *
+ * Description: The function prints the details of a struct dog. If
+ * any member of the struct is NULL, it prints (nil) instead of the
+ * member's value. If the pointer to the struct dog is NULL, it does
+ * nothing.
+ */
+
 void print_dog(struct dog *d);
+/**
+ * print_dog - Function that prints a struct dog
+ * @d: pointer to the dog structure
+ *
+ * Description: The function will print the details of a struct dog.
+ * If an element of d is NULL, print (nil) instead of this element.
+ *  (if name is NULL, print Name: (nil)).
+ *  If d is NULL print nothing.
+ */
+
 #endif /* DOG_H */

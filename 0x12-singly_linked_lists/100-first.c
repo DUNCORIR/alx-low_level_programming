71a1cb07b7a641fc6fd18a17f563c05604a3a584
duncorir <duncorir@mail.com>
2024-08-02 09:29:08 +0300

    Function that prints ..

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-/* Is a function that prints the required message */
+/**
+ * print_before_main - Function that prints specific message
+ * before main function is executed.
+ *
+ * Description:_attribute_((constructor)): Makes sure function is called
+ * before main function.
+ */
 void __attribute__ ((constructor)) print_before_main()
 {
 	printf("You're beat! and yet, you must allow,\n");

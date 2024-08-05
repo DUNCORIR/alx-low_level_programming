afaa069d9d5a5f49a4be84c64f9b1c172aaf6c85
duncorir <duncorir@mail.com>
2024-08-05 18:22:57 +0300

    function that returns the number of elements in a linked listint_t list.

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -3,9 +3,9 @@
 
 /**
  * listint_len - Function that returns the number in a linked listint_t list.
- * @h: The pointer to the head of the linked list.
+ * @h: Pointer to the head of the linked list.
  *
- * Return: Returns the number of elements in a linked list.
+ * Return: The number of elements in a linked list.
  */
 size_t listint_len(const listint_t *h)
 {

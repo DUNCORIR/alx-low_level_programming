992dd53e5cc02de538003ff03e170309a3ba97b4
duncorir <duncorir@mail.com>
2024-08-17 14:08:35 +0300

    posix

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /* For size_t*/
 #include <stddef.h>
 /* For open, O_RDONLT */
@@ -8,17 +11,10 @@
 /* For close,read,write */
 #include <unistd.h>
 
-void print_error(const char *message)
-{
-	fprintf(stderr, "%s\n", message);
-	exit(98);
-}
-
-int _putchar(char c);
-
 /*****************FUNCTIONS*********/
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-
+void print_error(const char *message);
+int _putchar(char c);
 #endif /* MAIN.H */

95e9d9080a41bccea858b92768282176cb70fcb4
duncorir <duncorir@mail.com>
2024-08-17 14:18:11 +0300

    posix

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -15,6 +15,6 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void print_error(const char *message);
+void print_error(const char *message, int exit_code, const char *filename);
 int _putchar(char c);
 #endif /* MAIN.H */

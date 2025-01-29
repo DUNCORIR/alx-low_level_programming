19add09204bac318ca4f246e7c4ec125c5450517
duncorir <duncorir@mail.com>
2025-01-29 16:25:11 +0300

    linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -14,10 +14,12 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
+	i = 0;
+
 	if (array == NULL)
 		return (-1);
 
-	for (i = 0; i < size; i++)
+	for (i < size; i++)
 	{
 		printf("value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
